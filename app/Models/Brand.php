bb595a314b9e45053d48df62ab6695c4174d885b
rakib <phi.rakib@gmail.com>
2024-05-01 17:16:59 +0600

    added products relation in brand & category

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -12,4 +12,9 @@ class Brand extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
