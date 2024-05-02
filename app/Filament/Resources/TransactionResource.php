fb765c97c47cd8a12f6d614a123cda747df8c4b4
rakib <phi.rakib@gmail.com>
2024-05-02 12:43:48 +0600

    added transaction crud

---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Product;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('supplier_id')
+                    ->relationship('supplier', 'name')
+                    ->required(),
+
+                DateTimePicker::make('transact_at')
+                    ->label('Date')
+                    ->required(),
+
+                Repeater::make('productTransaction')
+                    ->relationship()
+                    ->schema([
+                        Select::make('product_id')
+                            ->relationship('product', 'name')
+                            ->required(),
+                        TextInput::make('quantity')
+                            ->required()
+                            ->numeric(),
+                        TextInput::make('unit_price')
+                            ->required()
+                            ->numeric()
+                            ->prefix('$'),
+                    ])
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(function (Get $get, Set $set) {
+                        self::updateTotals($get, $set);
+                    })->columnSpanFull(),
+
+                TextInput::make('total')
+                    ->numeric()
+                    ->readOnly()
+                    ->prefix('$')
+                    ->afterStateHydrated(function (Get $get, Set $set) {
+                        self::updateTotals($get, $set);
+                    }),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('#')
+                    ->rowIndex(),
+                TextColumn::make('supplier.name')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('product_transaction_count')
+                    ->counts('productTransaction')
+                    ->label('Total Products'),
+                TextColumn::make('total')
+                    ->label('Total Amount'),
+                TextColumn::make('productTransaction.product.name')
+                    ->listWithLineBreaks()
+                    ->bulleted(),
+                TextColumn::make('transact_at')
+                    ->label('Date')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+        ];
+    }
+
+    public static function updateTotals(Get $get, Set $set): void
+    {
+        $selectedProducts = collect($get('productTransaction'))->filter(fn ($item) => !empty($item['product_id']) && !empty($item['quantity']));
+
+        $subtotal = $selectedProducts->reduce(function ($subtotal, $product) {
+            return $subtotal + ($product['unit_price'] * $product['quantity']);
+        }, 0);
+
+        $set('total', number_format($subtotal, 2, '.', ''));
+    }
+}
