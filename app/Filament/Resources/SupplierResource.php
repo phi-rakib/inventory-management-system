d465301eb163484e3bc1746430162b866d3eab63
rakib <phi.rakib@gmail.com>
2024-05-02 18:18:39 +0600

    made created_at column visible by default

--- app/Filament/Resources/SupplierResource.php
+++ app/Filament/Resources/SupplierResource.php
@@ -44,7 +44,7 @@ class SupplierResource extends Resource
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->toggleable(isToggledHiddenByDefault: false),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
