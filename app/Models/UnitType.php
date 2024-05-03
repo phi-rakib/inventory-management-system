bb0076e46eb320cf94c44bac5ae5e5a6bf6f13d2
rakib <phi.rakib@gmail.com>
2024-05-03 10:31:25 +0600

    added unit_types model, factory & seeder

---
+++ app/Models/UnitType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UnitType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
