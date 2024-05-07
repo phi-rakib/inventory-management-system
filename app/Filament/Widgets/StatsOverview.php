32b21ffb1e9b06e350469aee71f9e202a5aa071d
rakib <phi.rakib@gmail.com>
2024-05-07 11:14:09 +0600

    fixed dashboard due widget calculation

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -24,9 +24,31 @@ class StatsOverview extends BaseWidget
             Stat::make('Supplier', Supplier::count()),
             Stat::make('Purchased', Transaction::sum('total')),
             Stat::make('Paid', Payment::sum('amount')),
-            Stat::make('Due', function() {
-                $due = Transaction::sum('total') - Payment::sum('amount');
-                return $due < 0 ? 0 : $due;
+            Stat::make('Due', function () {
+                $transactions = Transaction::groupBy('supplier_id')
+                    ->selectRaw('sum(total) as sum, supplier_id')
+                    ->pluck('sum', 'supplier_id');
+                
+                $payments = Payment::groupBy('supplier_id')
+                    ->selectRaw('sum(amount) as sum, supplier_id')
+                    ->pluck('sum', 'supplier_id');
+                
+                $totalDue = 0;
+                foreach ($transactions as $supplier_id => $sum) {
+                    if(isset($payments[$supplier_id]))
+                    {
+                        $due = $sum - $payments[$supplier_id];
+                        if($due > 0)
+                        {
+                            $totalDue += $due;
+                        }
+                    } 
+                    else
+                    {
+                        $totalDue +=  $sum;
+                    }
+                }
+                return $totalDue;
             }),
         ];
     }
