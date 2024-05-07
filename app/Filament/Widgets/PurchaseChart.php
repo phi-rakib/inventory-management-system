ed968490cf69e4944e4700d406e029d36a033964
rakib <phi.rakib@gmail.com>
2024-05-07 13:28:21 +0600

    added purchase chart widget in the dashboard

---
+++ app/Filament/Widgets/PurchaseChart.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Transaction;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
+
+class PurchaseChart extends ChartWidget
+{
+    protected static ?string $heading = 'Purchase Chart By Date';
+
+    protected function getData(): array
+    {
+        $purchases = DB::table('transactions')
+            ->select(DB::raw('DATE(transact_at) as date'), DB::raw('sum(total) as sum'))
+            ->groupBy('date')
+            ->pluck('sum', 'date')
+            ->toArray();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Purchases',
+                    'data' => array_values($purchases),
+                ],
+            ],
+            'labels' => array_keys($purchases),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
