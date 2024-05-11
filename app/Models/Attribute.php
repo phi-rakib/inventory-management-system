4838c99daf5dbf4174d93dbd5ba7e2111dfe47fd
rakib <phi.rakib@gmail.com>
2024-05-11 18:21:28 +0600

    added attribute model, migration, seeder & factory

---
+++ app/Models/Attribute.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'attribute_product', 'attribute_id', 'product_id')
+            ->withPivot('attribute_value_id');
+    }
+}
