2584512d9ed0c07fed8196ea3768e09d5f919393
rakib <phi.rakib@gmail.com>
2024-05-15 03:20:39 +0600

    added deposit model & migration

---
+++ app/Models/Deposit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Deposit extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'account_id',
+        'deposit_date',
+        'amount',
+        'payment_method_id',
+    ];
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
+}
