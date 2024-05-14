5c852e2b91737b71f01d4fa83518257766b548cb
rakib <phi.rakib@gmail.com>
2024-05-14 10:06:54 +0600

    added product transfer curd

---
+++ app/Filament/Resources/TransferResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransferResource\Pages;
+use App\Filament\Resources\TransferResource\RelationManagers;
+use App\Models\Transfer;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransferResource extends Resource
+{
+    protected static ?string $model = Transfer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('from_warehouse_id')
+                    ->relationship(name: 'fromWarehouse', titleAttribute: 'name')
+                    ->required(),
+                Select::make('to_warehouse_id')
+                    ->relationship(name: 'toWarehouse', titleAttribute: 'name')
+                    ->required(),
+                Select::make('product_id')
+                    ->relationship(name: 'product', titleAttribute: 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('quantity')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('fromWarehouse.name')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('toWarehouse.name')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('product.name')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransfers::route('/'),
+            'create' => Pages\CreateTransfer::route('/create'),
+            'edit' => Pages\EditTransfer::route('/{record}/edit'),
+        ];
+    }
+}
