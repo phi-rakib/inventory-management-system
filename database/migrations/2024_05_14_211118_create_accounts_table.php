ef7201a2ab47e67f6ff7b06c528dd224733b0fa4
rakib <phi.rakib@gmail.com>
2024-05-15 03:35:43 +0600

    added column balance in accounts table

--- database/migrations/2024_05_14_211118_create_accounts_table.php
+++ database/migrations/2024_05_14_211118_create_accounts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('account_number');
+            $table->decimal('balance', 8, 2);
             $table->text('description')->nullable();
             $table->timestamps();
         });
