43d99e45198a8037cdcd1ce41b4f0fef85750ee9
rakib <phi.rakib@gmail.com>
2024-05-15 03:42:47 +0600

    added deposit crud

---
+++ app/Filament/Resources/DepositResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DepositResource\Pages;
+use App\Filament\Resources\DepositResource\RelationManagers;
+use App\Models\Deposit;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DepositResource extends Resource
+{
+    protected static ?string $model = Deposit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                DatePicker::make('deposit_date')
+                    ->label('Date')
+                    ->required(),
+                Select::make('account_id')
+                    ->relationship(name: 'account', titleAttribute: 'name')
+                    ->required(),
+                Select::make('deposit_category_id')
+                    ->relationship(name: 'depositCategory', titleAttribute: 'name')
+                    ->required(),
+                TextInput::make('amount')
+                    ->required()
+                    ->numeric(),
+                Select::make('payment_method_id')
+                    ->relationship(name: 'paymentMethod', titleAttribute: 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('account.name')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('deposit_category.name')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('deposit_date')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('amount')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('payment_method.name')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDeposits::route('/'),
+            'create' => Pages\CreateDeposit::route('/create'),
+            'edit' => Pages\EditDeposit::route('/{record}/edit'),
+        ];
+    }
+}
