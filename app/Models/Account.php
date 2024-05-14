5b69fe95fa270afaa4172c5f2c8afba1c12968da
rakib <phi.rakib@gmail.com>
2024-05-15 03:13:56 +0600

    added account model & migration

---
+++ app/Models/Account.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'account_number',
+        'description',
+    ];
+}
