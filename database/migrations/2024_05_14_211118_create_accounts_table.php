ec543fff305d90a5bf3b2841b0adb3f5464db8e5
rakib <phi.rakib@gmail.com>
2024-05-15 18:15:26 +0600

    added balance default value zero in accounts table

--- database/migrations/2024_05_14_211118_create_accounts_table.php
+++ database/migrations/2024_05_14_211118_create_accounts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('account_number');
-            $table->decimal('balance', 8, 2);
+            $table->decimal('balance', 8, 2)->default(0);
             $table->text('description')->nullable();
             $table->timestamps();
         });
