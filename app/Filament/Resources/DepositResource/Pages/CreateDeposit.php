3ca595cb4bf17b484886b34724fb89d62ebfc68c
rakib <phi.rakib@gmail.com>
2024-05-15 18:13:54 +0600

    updated account balance on deposit create

--- app/Filament/Resources/DepositResource/Pages/CreateDeposit.php
+++ app/Filament/Resources/DepositResource/Pages/CreateDeposit.php
@@ -3,10 +3,20 @@
 namespace App\Filament\Resources\DepositResource\Pages;
 
 use App\Filament\Resources\DepositResource;
+use App\Models\Account;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDeposit extends CreateRecord
 {
     protected static string $resource = DepositResource::class;
+
+    protected function afterCreate(): void
+    {
+        $deposit = $this->record;
+        
+        $account = Account::find($deposit->account_id);
+        $account->balance += $deposit->amount;
+        $account->save();
+    }
 }
