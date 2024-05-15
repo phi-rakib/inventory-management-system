117fb29fd30d511d63e1ad1deb4ecf0a6d05c67c
rakib <phi.rakib@gmail.com>
2024-05-15 18:16:51 +0600

    updated account balance on expense create

--- app/Filament/Resources/ExpenseResource/Pages/CreateExpense.php
+++ app/Filament/Resources/ExpenseResource/Pages/CreateExpense.php
@@ -3,10 +3,20 @@
 namespace App\Filament\Resources\ExpenseResource\Pages;
 
 use App\Filament\Resources\ExpenseResource;
+use App\Models\Account;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateExpense extends CreateRecord
 {
     protected static string $resource = ExpenseResource::class;
+
+    protected function afterCreate(): void
+    {
+        $expense = $this->record;
+        
+        $account = Account::find($expense->account_id);
+        $account->balance -= $expense->amount;
+        $account->save();
+    }
 }
