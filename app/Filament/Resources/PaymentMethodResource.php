b47511c33db00fa104f1d77ff3825bed00f121ed
rakib <phi.rakib@gmail.com>
2024-05-16 20:43:30 +0600

    changed payment method menu icon

--- app/Filament/Resources/PaymentMethodResource.php
+++ app/Filament/Resources/PaymentMethodResource.php
@@ -17,7 +17,7 @@ class PaymentMethodResource extends Resource
 {
     protected static ?string $model = PaymentMethod::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
 
     protected static ?int $navigationSort = 4;
 
