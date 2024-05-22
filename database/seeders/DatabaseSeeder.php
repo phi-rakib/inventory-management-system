9e3ae93b91c19edc927eb350551a3bf70da6779e
rakib <phi.rakib@gmail.com>
2024-05-22 18:11:40 +0600

    added brand seeder & called it from database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             ExpenseSeeder::class,
             AttributeSeeder::class,
             AttributeValueSeeder::class,
+            BrandSeeder::class,
         ]);
     }
 }
