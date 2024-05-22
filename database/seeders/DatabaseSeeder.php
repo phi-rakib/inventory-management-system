425af7c2101e24391b6662c3e9a0bc75473b2eaf
rakib <phi.rakib@gmail.com>
2024-05-22 15:15:01 +0600

    added expenseCategory factory & seeder and called the seeder from database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\DepositCategory;
+use App\Models\ExpenseCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
             DepositCategorySeeder::class,
             PaymentMethodSeeder::class,
             DepositSeeder::class,
+            ExpenseCategorySeeder::class,
         ]);
     }
 }
