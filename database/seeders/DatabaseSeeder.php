a5bcee79d4876ba5a8124fbd89e0891f906a1fb3
rakib <phi.rakib@gmail.com>
2024-05-22 15:10:01 +0600

    added paymentMethod factory & seeder and called the seeder from database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AccountSeeder::class,
             DepositCategorySeeder::class,
+            PaymentMethodSeeder::class,
         ]);
     }
 }
