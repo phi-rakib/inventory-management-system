d97a728db649f51668e82045eb4849bc0b3af9bd
rakib <phi.rakib@gmail.com>
2024-05-22 15:02:15 +0600

    added depositCategory factory & seeder and called the seeder from database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\DepositCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AccountSeeder::class,
+            DepositCategorySeeder::class,
         ]);
     }
 }
